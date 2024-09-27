8ce79ab69192612c3375d985e66a07674c10be2b
Sajid Hussain <107537308+Sajid8686@users.noreply.github.com>
2024-09-27 16:13:20 +0500

    Update settings.py

--- textutils/settings.py
+++ textutils/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8x%h!cyynev2jjrhtm-z9rm8g^uc%ktf3o2we*+ga!%)i8rbt9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['https://textutils-zeta-three.vercel.app/']
 
