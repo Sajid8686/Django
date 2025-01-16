699505458b9b910f0bb7415c5e56beeeee30b416
Sajid Hussain <107537308+Sajid8686@users.noreply.github.com>
2025-01-16 20:59:16 +0500

    Update settings.py

--- textutils/settings.py
+++ textutils/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8x%h!cyynev2jjrhtm-z9rm8g^uc%ktf3o2we*+ga!%)i8rbt9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
